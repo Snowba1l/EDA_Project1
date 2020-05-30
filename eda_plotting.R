534c1b8655a289ccadd31b7d44a3a8eb404e3a20
Snowba1l <cnu_cx@126.com>
2020-05-30 22:57:38 +0800

    first commit download and unzip data

---
+++ eda_plotting.R
@@ -0,0 +1,5 @@
+# download and unzip data
+if(!file.exists('data')) dir.create('data')
+fileUrl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+download.file(fileUrl, destfile = './data/household_power_consumption.zip')
+unzip('./data/household_power_consumption.zip', exdir = './data')
